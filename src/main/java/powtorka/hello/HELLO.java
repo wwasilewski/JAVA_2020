391bceaedd4fe1a0afc20f3e16160d61d6f353eb
Wojciech <w.wasilewski@gmail.com>
2020-11-20 18:44:46 +0100

    testing vcs options

--- src/main/java/powtorka/hello/HELLO.java
+++ src/main/java/powtorka/hello/HELLO.java
@@ -5,5 +5,8 @@ public class HELLO {
     public static void main(String[] args) {
 
         System.out.println("HELLO czy nie HELLO");
+        System.out.println("Jim");
+        System.out.println(2);
+        System.out.println(Math.PI);
     }
 }
