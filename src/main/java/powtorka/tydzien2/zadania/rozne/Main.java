1af4e15f7080210f58c6fc66d27137e8e9f731b0
Wojciech <w.wasilewski@gmail.com>
2021-01-28 17:17:16 +0100

    add Zad38 from Rozne

---
+++ src/main/java/powtorka/tydzien2/zadania/rozne/Main.java
@@ -0,0 +1,9 @@
+package powtorka.tydzien2.zadania.rozne;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Zad38 zad38 = new Zad38();
+        zad38.printCircleArea();
+    }
+}
---
+++ src/main/java/powtorka/tydzien2/zadania/rozne/Zad38.java
@@ -0,0 +1,39 @@
+/*
+Napisz program obliczający pole powierzchni koła. Promień koła użytkownik
+wprowadza z klawiatury. Program powinien zasygnalizować błędne dane (liczbę
+ujemną lub zero) i ponownie zapytac o długośc promienia. Wykorzystaj metodę
+Parse klasy float. Oczywiście nie zapomnij o przechwyceniu ewentualnych wyjątków.
+*/
+
+package powtorka.tydzien2.zadania.rozne;
+
+import java.util.Scanner;
+
+public class Zad38 {
+
+    Scanner sc = new Scanner(System.in);
+
+    public double circleArea(float radius) {
+        return Math.PI * radius * radius;
+    }
+
+    public void printCircleArea() {
+        System.out.println("pick circle's radius:");
+        float radius;
+
+        while (true) {
+            try {
+                radius = Float.parseFloat(sc.nextLine());
+                if (radius > 0) {
+                    System.out.println("circle'a area: " + circleArea(radius));
+                    break;
+                } else {
+                    System.out.println("radius has to be > 0");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("wrong input data, pick again");
+            }
+        }
+        sc.close();
+    }
+}
