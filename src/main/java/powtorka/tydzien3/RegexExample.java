1c480394b9e4cee5e96ae5af3e424cf7a69cef04
Wojciech <w.wasilewski@gmail.com>
2021-01-29 18:17:31 +0100

    add RegexExample class

---
+++ src/main/java/powtorka/tydzien3/RegexExample.java
@@ -0,0 +1,30 @@
+package powtorka.tydzien3;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexExample {
+    public static void main(String[] args) {
+        // * - Occurs zero or more times, is short for {0,}
+        Pattern p = Pattern.compile("a*b");
+        Matcher m = p.matcher("aaaaab");
+        boolean b = m.matches();
+        System.out.println(b);
+
+        String pattern = "[A-Z][a-z]{2}";
+        String text = "Ala ma kota, a kot ma Ale";
+        // *** ma kota , a kot ma ***
+        System.out.println(text.replaceAll(pattern, "***"));
+
+        String textToSplit = "Jan;Marian;Kowalski;Magazynowa;14;12";
+        String splitPattern = ";";
+        String[] splittedText = textToSplit.split(splitPattern);
+        for (String s : splittedText) {
+            System.out.print(s + " ");
+        }
+
+        pattern = ".*(\\d{9})";
+        text = "Jan Kowalski 505666333";
+        System.out.println("\n" + text.replaceAll(pattern, "$1"));
+    }
+}
