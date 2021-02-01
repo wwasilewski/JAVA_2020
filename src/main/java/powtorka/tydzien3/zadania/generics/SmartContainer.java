44af336cd208137eb6bd27810c2f4620610d5534
Wojciech <w.wasilewski@gmail.com>
2021-02-01 14:13:27 +0100

    add generics SmartContainer example

---
+++ src/main/java/powtorka/tydzien3/zadania/generics/SmartContainer.java
@@ -0,0 +1,30 @@
+package powtorka.tydzien3.zadania.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SmartContainer<T> {
+    private List<T> elements = new ArrayList<>();
+
+    public void addToList(T element) {
+        elements.add(element);
+    }
+
+    public T getFromList(int index) {
+        return elements.get(index);
+    }
+
+    public void updateElement(int index, T element) {
+        elements.set(index, element);
+    }
+
+    public void printElement(int index) {
+        System.out.println("this is element no." + index + ": " + getFromList(index));
+    }
+
+    public void printAll() {
+        for (T e : elements) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ src/main/java/powtorka/tydzien3/zadania/generics/SmartContainerMain.java
@@ -0,0 +1,24 @@
+package powtorka.tydzien3.zadania.generics;
+
+public class SmartContainerMain {
+    public static void main(String[] args) {
+        SmartContainer<String> stringSmartContainer = new SmartContainer<>();
+        stringSmartContainer.addToList("this");
+        stringSmartContainer.addToList("is");
+        stringSmartContainer.addToList("my");
+        stringSmartContainer.addToList("string");
+        stringSmartContainer.printAll();
+        stringSmartContainer.printElement(3);
+        stringSmartContainer.updateElement(3, "newstring");
+        stringSmartContainer.printElement(3);
+
+        SmartContainer<Integer> integerSmartContainer = new SmartContainer<>();
+        integerSmartContainer.addToList(1);
+        integerSmartContainer.addToList(2);
+        integerSmartContainer.addToList(3);
+        integerSmartContainer.printAll();
+        integerSmartContainer.printElement(2);
+        integerSmartContainer.updateElement(2, 4);
+        integerSmartContainer.printElement(2);
+    }
+}
