fce8a4842c4fdbe946d6e20ceebc40bf85e2f914
Wojciech <w.wasilewski@gmail.com>
2021-02-02 23:29:10 +0100

    add Zad16 from Programowanie wspolbiezne

---
+++ src/main/java/powtorka/tydzien3/zadania/threads/Zad16Main.java
@@ -0,0 +1,8 @@
+package powtorka.tydzien3.zadania.threads;
+
+public class Zad16Main {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new Zad16());
+        thread.start();
+    }
+}
