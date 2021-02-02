2fb2660aa0556a312e15049b8def156803023cda
Wojciech <w.wasilewski@gmail.com>
2021-02-02 23:17:08 +0100

    add Zad15 from Programowanie wspolbiezne

---
+++ src/main/java/powtorka/tydzien3/zadania/threads/Zad15.java
@@ -0,0 +1,28 @@
+/*
+Przygotuj aplikację uruchamiającą 1 wątek wyświetlający losową liczbę z zakresu
+10-100. Wątek powinien wyświetlić liczbę 5 razy w odstępie 1 sekundy. Przygotuj
+wątek poprzez dziedziczenie z klasy Thread.
+*/
+
+package powtorka.tydzien3.zadania.threads;
+
+import java.util.Random;
+
+public class Zad15 extends Thread {
+    private static final int TIMEOUT = 1000;
+    private Random random = new Random();
+
+    @Override
+    public void run() {
+        int counter = 0;
+        try {
+            while (counter < 5) {
+                System.out.println(random.nextInt(91) + 10);
+                counter++;
+                Thread.sleep(TIMEOUT);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/powtorka/tydzien3/zadania/threads/Zad15Main.java
@@ -0,0 +1,8 @@
+package powtorka.tydzien3.zadania.threads;
+
+public class Zad15Main {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Zad15();
+        thread.start();
+    }
+}
