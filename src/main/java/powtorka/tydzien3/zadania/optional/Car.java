afc2506714a7cefb698c861ffee58ed40e4eb403
Wojciech <w.wasilewski@gmail.com>
2021-02-05 17:17:25 +0100

    add Zad25 from Optional

---
+++ src/main/java/powtorka/tydzien3/zadania/optional/Car.java
@@ -0,0 +1,26 @@
+package powtorka.tydzien3.zadania.optional;
+
+public class Car {
+    /**
+     * Car price
+     */
+    private Double price;
+
+    /**
+     * Creates car instance
+     *
+     * @param price Car price
+     */
+    public Car(Double price) {
+        this.price = price;
+    }
+
+    /**
+     * Returns car price
+     *
+     * @return Car price
+     */
+    public Double getPrice() {
+        return price;
+    }
+}
