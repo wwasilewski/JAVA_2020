d4f30354ab5a2dc469e406abac6069d9c35982c1
Wojciech <w.wasilewski@gmail.com>
2021-02-22 20:39:47 +0100

    add ex3 from Coderslab Day2 - Homework

---
+++ src/main/java/powtorka/coderslab/day2/homework/ex3/Ex3.java
@@ -0,0 +1,28 @@
+/*
+static String upperCase(String str, int index)
+Metoda zwraca napis str, w którym znaki zostały zamienione na duże z pozycji
+podzielnych przez `index`
+*/
+
+package powtorka.coderslab.day2.homework.ex3;
+
+public class Ex3 {
+    public static void main(String[] args) {
+        String str = "abcdefghij";
+        System.out.println(upperCase(str, 2));
+
+    }
+
+    public static String upperCase(String str, int index) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (i != 0 && i % index == 0) {
+                c = Character.toUpperCase(c);
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
