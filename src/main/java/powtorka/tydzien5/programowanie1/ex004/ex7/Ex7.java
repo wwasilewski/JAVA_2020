96c1983cf400d15f4bbc015e51b369d4efd8fef3
Wojciech <w.wasilewski@gmail.com>
2021-02-23 19:05:45 +0100

    add ex7 from Zadania004 - Stream

--- src/main/java/powtorka/tydzien5/programowanie1/ex004/ex7/Ex7.java
+++ src/main/java/powtorka/tydzien5/programowanie1/ex004/ex7/Ex7.java
@@ -6,18 +6,19 @@ do 2 miejsca po przecinku.
 
 package powtorka.tydzien5.programowanie1.ex004.ex7;
 
+import java.util.Arrays;
+
 public class Ex7 {
     public static void main(String[] args) {
         int[] numbers = new int[]{148, 105, 36, 37, 56, 132, 121, 36, 180,
                 8, 61, 171, 180, 6, 65, 0, 66, 58, 162, 25, 128, 35, 32, 4, 104,
                 6, 75, 31, 66, 125, 188, 159, 121, 61, 173, 188, 34, 141, 182,
                 192, 18, 18, 165, 136, 76, 64, 41, 195, 147, 74};
-        System.out.println(sqrtFromAllNumbers(numbers));
+        sqrtFromAllNumbers(numbers);
     }
 
-    public static double sqrtFromAllNumbers(int[] numbers) {
-        return 1d;
-
-
+    public static void sqrtFromAllNumbers(int[] numbers) {
+        Arrays.stream(numbers)
+                .forEach(element -> System.out.printf(" %.2f", Math.sqrt(element)));
     }
 }
