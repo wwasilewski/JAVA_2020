ebabc06c1a12001fe7a104b58024315a68da8d58
Wojciech <w.wasilewski@gmail.com>
2021-03-05 10:44:47 +0100

    working on ex6 in Zadania009 - ROZNE (in progress)

--- src/main/java/powtorka/tydzien5/programowanie1/ex009/ex6/Main.java
+++ src/main/java/powtorka/tydzien5/programowanie1/ex009/ex6/Main.java
@@ -1,17 +1,22 @@
 package powtorka.tydzien5.programowanie1.ex009.ex6;
 
-import powtorka.TextColor;
-import powtorka.Utils;
-
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String pathToDir = Utils.displayTextAndGetValue("provide path: ", true);
-        Integer depthToCheck = Utils.displayTextAndGetInt("provide depth to check: ");
+//        String pathToDir = Utils.displayTextAndGetValue("provide path: ", true);
+//        Integer depthToCheck = Utils.displayTextAndGetInt("provide depth to check: ");
         var showFiles = new ShowFilesFromDirectories();
-        Utils.displayTextOnConsole("main dir: " + pathToDir, TextColor.ANSI_BLUE);
-        showFiles.listFilesForFolder(new File(pathToDir), depthToCheck);
+//        Utils.displayTextOnConsole("main dir: " + pathToDir, TextColor.ANSI_BLUE);
+//        showFiles.listFilesForFolder(new File(pathToDir), depthToCheck);
+
+        //to powyzej dziala
+
+        List<String> list = showFiles.listFilesForFolder2(new File("D://test"), 2);
+//        for (String l : list) {
+//            System.out.println(l);
+//        }
     }
 }
--- src/main/java/powtorka/tydzien5/programowanie1/ex009/ex6/ShowFilesFromDirectories.java
+++ src/main/java/powtorka/tydzien5/programowanie1/ex009/ex6/ShowFilesFromDirectories.java
@@ -1,8 +1,16 @@
 package powtorka.tydzien5.programowanie1.ex009.ex6;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ShowFilesFromDirectories {
+
+    private List<String> listOfFiles;
+
+    public ShowFilesFromDirectories() {
+        this.listOfFiles = new ArrayList<>();
+    }
 
     public void listFilesForFolder(File folder, int depth) {
         for (File fileEntry : folder.listFiles()) {
@@ -16,5 +24,21 @@ public class ShowFilesFromDirectories {
                 System.out.println(fileEntry.getName() + " " + fileEntry.length() / 1024 + " KB");
             }
         }
+    }
+
+    public List<String> listFilesForFolder2(File folder, int depth) {
+        for (File fileEntry : folder.listFiles()) {
+            if (fileEntry.isDirectory()) {
+                if (depth > 0) {
+                    depth--;
+                    System.out.println("files in: " + fileEntry.getAbsolutePath());
+                    listFilesForFolder2(fileEntry, depth);
+                }
+            } else {
+                System.out.println(fileEntry.getName() + " " + fileEntry.length() / 1024 + " KB");
+                listOfFiles.add(fileEntry.getName() + " " + fileEntry.length() / 1024 + " KB");
+            }
+        }
+        return listOfFiles;
     }
 }
